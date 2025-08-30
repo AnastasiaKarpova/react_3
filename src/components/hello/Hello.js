1ee1760a3fbf66bb6835720a523157dd1a9dd270
Anastasia <karpovanastiya@mail.ru>
2025-08-30 21:37:40 +0300

    State

--- src/components/hello/Hello.js
+++ src/components/hello/Hello.js
@@ -6,12 +6,22 @@ class Hello extends React.Component
     constructor(props)
     {
         super(props);
-        this.state = {text: "Hello"}
+        this.state = {
+            text: "Hello",
+            sign: "Eng"
+        }
+    }
+    changeText = () =>{
+        this.setState({text: this.state.text === "Hello" ? "Привет" : "Hello"});
+        this.setState({sign: this.state.sign === "Rus" ? "Eng" : "Rus"});
     }
     render()
     {
         return(
-            <div className='Hello'>{this.state.text}</div>
+            <div>
+                <button onClick={this.changeText}>Изменить</button>
+                <div className='Hello'>{this.state.text}</div>
+            </div>
         );
     }
 }
