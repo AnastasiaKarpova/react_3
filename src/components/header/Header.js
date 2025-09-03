c25308f70d1d501477bc2b47e16ac902e0d4945a
Anastasia <karpovanastiya@mail.ru>
2025-09-03 22:51:57 +0300

    State day 2

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -19,7 +19,7 @@ class Header extends React.Component{
                     >
                       Learn React
                     </a>
-                  </header>
+              </header>
         )
     }
 }
